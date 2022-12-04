8edcd138b2b6d165a1e1c2d204149ca9fa16655c
david <davidklapper@LAPTOP-MQU5U4H4.localdomain>
2022-12-04 14:46:23 -0800

    adding elements that are from tutorial on https://www.digitalocean.com/community/tutorials/how-to-use-templates-in-a-flask-application, as part of this base of code

--- david.py
+++ david.py
@@ -5,12 +5,17 @@
 ## it. The interpreter will ignore all lines beginning with at least one
 ## of the "comment character", the pound or hashtag symbol, #
 
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def root_site():
+    return render_template('index.html',utc_dt=datetime.datetime.utcnow())
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 app.run(port=5004)
